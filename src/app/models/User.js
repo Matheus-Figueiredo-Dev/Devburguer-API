7da884f37c91fdebe35a53ce26be8b4bf28af658
MatheusFigueiredo1001 <figueiredom924@gmail.com>
2025-03-12 19:17:00 -0300

    First commit

---
+++ src/app/models/User.js
@@ -0,0 +1,33 @@
+import { Model, Sequelize } from 'sequelize';
+import bcrypt from 'bcrypt';
+
+class User extends Model {
+	static init(sequelize) {
+		// biome-ignore lint/complexity/noThisInStatic: <explanation>
+		super.init(
+			{
+				name: Sequelize.STRING,
+				email: Sequelize.STRING,
+				password: Sequelize.VIRTUAL, //Não existe no banco de dados, por isso é virtual
+				password_hash: Sequelize.STRING,
+				admin: Sequelize.BOOLEAN,
+			},
+			{
+				sequelize,
+			},
+		);
+		// biome-ignore lint/complexity/noThisInStatic: <explanation>
+		this.addHook('beforeSave', async (user) => {
+			if (user.password) {
+				user.password_hash = await bcrypt.hash(user.password, 10);
+			}
+		});
+		// biome-ignore lint/complexity/noThisInStatic: <explanation>
+		return this;
+	}
+	async checkPassword(password) {
+		return bcrypt.compare(password, this.password_hash);
+	}
+}
+
+export default User;
