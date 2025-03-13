9ce31676a6079c27866efabcf1760933d21b2dba
MatheusFigueiredo1001 <figueiredom924@gmail.com>
2025-03-13 14:56:19 -0300

    Criando conexão com mongo

--- src/database/index.js
+++ src/database/index.js
@@ -1,8 +1,9 @@
 import { Sequelize } from 'sequelize';
 import configDatabase from '../config/database';
+import mongoose from 'mongoose';
 
 import User from '../app/models/User';
-import Product  from '../app/models/Product';
+import Product from '../app/models/Product';
 import Category from '../app/models/Category';
 
 const models = [User, Product, Category];
@@ -16,14 +17,18 @@ class Database {
 	init() {
 		this.connection = new Sequelize(configDatabase);
 		models
-		.map((model) => model.init(this.connection))
-		.map(
-			// biome-ignore lint/complexity/useOptionalChain: <explanation>
-			(model) => model.associate && model.associate(this.connection.models),
-		);
+			.map((model) => model.init(this.connection))
+			.map(
+				// biome-ignore lint/complexity/useOptionalChain: <explanation>
+				(model) => model.associate && model.associate(this.connection.models),
+			);
 	}
 
-	mongo() {}
+	mongo() {
+		this.mongoConnection = mongoose.connect(
+			'mongodb://localhost:27017/devburger',
+		);
+	}
 }
 
 export default new Database();
